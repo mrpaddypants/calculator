e16158fd2bea5a0a0d76057563adfe5e4a826359
mrpaddypants <patrick.wuethrich97@gmail.com>
2024-11-15 20:16:58 +0100

    add functions for basic operators

---
+++ script.js
@@ -0,0 +1,21 @@
+// operator functions
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return false;
+    } else {
+        return num1 / num2;
+    }
+}
