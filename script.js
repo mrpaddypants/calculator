eaa327e2005daaf9ccf39c83583b00c3c0649c1b
mrpaddypants <patrick.wuethrich97@gmail.com>
2024-11-17 15:23:27 +0100

    add keyboard feature and add back button functionality

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let calcNum = 0;
 let calcOperator = "";
+let displayScrnValue = "";
+let result = 0;
 
+function clearScrn() {
+    location.reload();
+}
+
+function back() {
+    displayScrnValue = displayScrn.textContent;
+    displayScrnValue = displayScrnValue.slice(0, -1);
+    displayScrn.textContent = displayScrnValue;
+}
 
 
 // number buttons on the calculator
@@ -10,7 +21,6 @@ numBtns.forEach((numBtn) => {
         displayScrn.textContent += numBtn.textContent;
         calcNum = numBtn.textContent;
         calcNum = Number(calcNum);
-        console.log(calcNum);
     });
 })
 
@@ -28,14 +38,54 @@ operatorBtns.forEach((oprBtn) => {
 
 // other entities
 const clearBtn = document.querySelector(".clearBtn");
+clearBtn.addEventListener("click", () => {
+    clearScrn();
+})
 const backBtn = document.querySelector(".backBtn");
+backBtn.addEventListener("click", () => {
+    back();
+});
+
 const dotBtn = document.querySelector(".dotBtn");
 const displayScrn = document.querySelector(".display");
+document.addEventListener("keydown", (e) => {
+    displayScrn.textContent += e.key;
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+        calcOperator = e.key;
+    } else if (e.key === 0 || e.key === 1 || e.key === 2 || e.key === 3 || e.key === 4 || e.key === 5 || e.key === 6 || e.key === 7 || e.key === 8 || e.key === 9) {
+        calcNum = e.key;
+    } else if (e.key === "Enter") {
+        let arrayOfNums = displayScrn.textContent.split(calcOperator).map(function(item) {
+            return parseInt(item, 10);
+        });
+        if (arrayOfNums.length < 2) {
+            displayScrn.textContent = "Error";
+            location.reload();
+        } else {
+            operate(arrayOfNums);
+        }
+    } else if (e.key === "Backspace") {
+        back();
+    }
+    
+})
 const equalBtn = document.querySelector(".equalBtn");
 
+equalBtn.addEventListener("click", () => {
+    let arrayOfNums = displayScrn.textContent.split(calcOperator).map(function(item) {
+        return parseInt(item, 10);
+    });
+    if (arrayOfNums.length < 2) {
+        displayScrn.textContent = "Error";
+        location.reload();
+    } else {
+        operate(arrayOfNums);
+    }
+});
 
 
-let result = 0;
+
+
 
 function operate(array) {
         if (calcOperator === "+") {
@@ -48,7 +98,7 @@ function operate(array) {
                 return total - next;
             });
             displayScrn.textContent = result;
-        } else if (calcOperator === "x") {
+        } else if (calcOperator === "*") {
             result = array.reduce((total, next) => {
                 return total * next;
             });
@@ -65,21 +115,5 @@ function operate(array) {
         }
     }
 
-function clearScrn() {
-    clearBtn.addEventListener("click", () => {
-        location.reload();
-    });
-}
-clearScrn();
 
-equalBtn.addEventListener("click", () => {
-    let arrayOfNums = displayScrn.textContent.split(calcOperator).map(function(item) {
-        return parseInt(item, 10);
-    });
-    if (arrayOfNums.length < 2) {
-        alert("ERROR");
-        location.reload();
-    } else {
-        operate(arrayOfNums);
-    }
-});
+
